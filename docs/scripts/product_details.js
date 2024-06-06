e732b89d67f6bdddb366f1d62a5b223de5efb8fb
lyhoung <pov.lyhoung@e-power.com.kh>
2024-06-06 11:00:11 +0700

    ++

--- docs/scripts/product_details.js
+++ docs/scripts/product_details.js
@@ -76,24 +76,76 @@ class ProductDetailScript {
         });
     }
 
-    addToCard(itme) {
+    addToCard(item) {
         const waitHmlElement = setInterval(() => {
             const addCard = document.querySelector("#add-to-card");
+            const alertNoti = document.getElementById("alert-noti");
+            const message = document.getElementById("message");
+            var id = "";
             if (addCard) {
                 clearInterval(waitHmlElement);
-                addCard.addEventListener('click', () => {
-                    if (itme) {
-                        console.log(itme)
+                addCard.addEventListener('click', async () => {
+                    if(id === item.id){
+                        alertNoti.style.right = "-20px";
+                        alertNoti.style.backgroundColor = "#FFA27F";
+                        this.changeIconMsg({isSuccess: false});
+                        await setTimeout(() => {
+                            alertNoti.style.right = "-400px";
+                        }, 7000);
+                        return console.log("product already added to card");
+                    }
+
+                    if (item) {
+                        console.log(item)
                         const m = new Menu();
                         m.setCounter({isCount: true});
+                        id = item.id;
+                        addCard.innerHTML = "Cancel Card";
+                        alertNoti.style.right = "-20px";
+                        alertNoti.style.backgroundColor = "#40A2E3";
+                        this.changeIconMsg({isSuccess: true});
+                        await setTimeout(() => {
+                            alertNoti.style.right = "-400px";
+                        }, 7000);
+                        return console.log("added success");
                     }
-                    
                 });
             } else {
                 console.error('Element with ID "add-to-card" not found');
             }
         });
         
+    }
+
+    changeIconMsg({isSuccess  = false}){
+        const waitting = setInterval(() => {
+            const alert = document.getElementById("alert-noti");
+            if(alert){
+                clearInterval(waitting);
+
+                if(!isSuccess){
+                    let icon = `
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                        </svg>
+                        &nbsp;&nbsp;
+                        <div id="message">Product already added to card!</div>
+                    `;
+                    return alert.innerHTML = icon;
+                }
+    
+                if(isSuccess){
+                    let icon = `
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="green" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                        </svg>
+                        &nbsp;&nbsp;
+                        <div id="message">Add successfully!</div>
+                    `;
+                    return alert.innerHTML = icon;
+                }
+            }
+        });
     }
 }
 
